029c37c77dbc2b2993ce5b169b43828d0d0ae395
Nicoletapa <112115960+Nicoletapa@users.noreply.github.com>
2023-04-17 11:54:12 +0200

    Add files via upload

--- mycrypt/mycrypt.go
+++ mycrypt/mycrypt.go
@@ -1,16 +1,18 @@
 package mycrypt
 
-var ALF_SEM03 []rune = []rune("abcdefghijklmnopqrstuvwxyzæøå0123456789.,:; ")
+var ALF_SEM03 []rune = []rune("ABCDEFGHIJKLMNOPQRSTUWXYZÆØÅabcdefghijklmnopqrstuvwxyzæøå0123456789.,:; ")
 
 func Krypter(melding []rune, alphabet []rune, chiffer int) []rune {
 	kryptertMelding := make([]rune, len(melding))
 	for i := 0; i < len(melding); i++ {
 		indeks := sokIAlfabetet(melding[i], alphabet)
-		if indeks+chiffer >= len(alphabet) {
+		if indeks+chiffer >= len(alphabet)  {
 			kryptertMelding[i] = alphabet[indeks+chiffer-len(alphabet)]
 		} else {
 			kryptertMelding[i] = alphabet[indeks+chiffer]
 		}
+		
+		
 	}
 	return kryptertMelding
 }
@@ -18,8 +20,9 @@ func Krypter(melding []rune, alphabet []rune, chiffer int) []rune {
 func sokIAlfabetet(symbol rune, alfabet []rune) int {
 	for i := 0; i < len(alfabet); i++ {
 		if symbol == alfabet[i] {
+			
 			return i
-			break
+			
 		}
 	}
 	return -1
